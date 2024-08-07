6a31e7b2dd5055c8258c53c1fe4f2a59cf32b9fd
Your NameMoyliyev <you@example.commoyliyevalibek2004@icloud.com>
2024-08-07 21:03:07 +0500

    init

--- src/start/modules.js
+++ src/start/modules.js
@@ -1,11 +1,14 @@
 const fileUpload = require('express-fileupload');
 const errorHandler = require('../middlewares/error.handler.middleware');
 const routes = require('../routes');
+const cors = require('cors')
 
 module.exports = async (app, express) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-
+  app.use(cors({
+    origin: "*",
+  }));
   app.use(fileUpload());
 
   app.use('/api', routes);
